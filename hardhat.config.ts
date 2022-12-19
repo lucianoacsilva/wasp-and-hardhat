d3de0b38e605de9398394c18f630684fd9604806
Luciano Augusto Campagnoli da Silva <luti4497@gmail.com>
2022-12-18 22:18:37 -0300

    Backup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -24,20 +24,26 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
+  defaultNetwork: "local",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
+    // ropsten: {
+    //   url: process.env.ROPSTEN_URL || "",
+    //   accounts:
+    //     process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    // },
+    local: {
+      url: process.env.LOCAL_URL,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      timeout: 60000
+    }
   },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
+  // gasReporter: {
+  //   enabled: process.env.REPORT_GAS !== undefined,
+  //   currency: "USD",
+  // },
+  // etherscan: {
+  //   apiKey: process.env.ETHERSCAN_API_KEY,
+  // },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Owner = await ethers.getContractFactory("Owner");
+  const owner = await Owner.deploy();
 
-  await greeter.deployed();
+  await owner.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Owner deployed to:", owner.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
